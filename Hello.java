be81686577d24b8ea1b5035e42a0895e769389ef
osorcom <oscar.sor.com@gmail.com>
2023-09-15 18:33:59 +0200

    compte enrera

--- Hello.java
+++ Hello.java
@@ -2,4 +2,10 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Hello");
     }
+
+    int count = 3;
+    while(count>0){
+        System.out.println("Compte enrera: "+count);
+        count --;
+    }
 }
