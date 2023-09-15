7231668957ccd0d68a5d16d95ac9521f69ed3efa
Miles <102043640+MilesVorkosigan@users.noreply.github.com>
2023-09-15 19:20:28 +0200

    modificaT

--- Hello.java
+++ Hello.java
@@ -1,15 +1,14 @@
-public class Hello{
-    public static void main(String[] args){
-        System.out.println("Miles, ja sóc aquí");
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Miles, ja soc aquí");
 
-        for(int i=0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.println(i);
-            
-    int count = 3;
-    while(count>0){
-        System.out.println("Compte enrera: "+count);
-        count --;
-    }
+        }
+        int count = 3;
+        while (count > 0) {
+            System.out.println("Compte enrera: " + count);
+            count--;
         }
     }
 
