ea0b4ccae04e63a8812aa3eb96433ca55e15e207
osorcom <oscar.sor.com@gmail.com>
2023-09-15 17:26:24 +0200

    llista nums

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello");
+
+        for(int i=0; i<5; i++){
+            System.out.println(i);
+        }
     }
 }
