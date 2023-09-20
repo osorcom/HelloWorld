8cd1b53d1dcbd9d5154af76731ee6d6ee58bc4d3
Miles <102043640+MilesVorkosigan@users.noreply.github.com>
2023-09-20 16:18:10 +0200

    canvis últims

--- Hello.java
+++ Hello.java
@@ -10,19 +10,15 @@ public class Hello {
             System.out.println("Compte enrera: " + count);
             count--;
         }
-    }
-
-<<<<<<< HEAD
-=======
-    int count = 3;
-    while(count>0){
-        System.out.println("Compte enrera: "+count);
-        count --;
-    }
-    System.out.println("[ PolNinja ]");
+            System.out.println("[ PolNinja ]");
 
     System.out.println("XD");
 
     System.out.println("SIUUUU");
+    }
+
+<<<<<<< HEAD
+=======
+
 >>>>>>> 8afa6c47a7b3ce871d9433f4da263ecf14bc4553
 }
